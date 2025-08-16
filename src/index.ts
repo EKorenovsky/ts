168ea85ee68f2f40a764f2de6696a96862458093
ekorenovsky <e_korenovsky@mail.ru>
2025-08-16 23:31:10 +0300

    check 1

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import * as path from "path";
+import Client from "ssh2-sftp-client";
+
+const sftpConfig = {
+  host: "95.216.200.243",
+  port: 22, // стандартный порт SFTP
+  username: "root",
+  password: "mKHc33PsmknUVks4JJTC",
+};
+
+const sftp = new Client();
+
+async function sftpOperations() {
+  try {
+    // Подключение к серверу
+    await sftp.connect(sftpConfig);
+    console.log("Connected to SFTP server");
+    const localFilePath = path.join(__dirname, "readme.txt");
+    console.log("Local file path", localFilePath);
+    const remoteFilePath = "/store/audio/readme.txt";
+    await sftp.put(localFilePath, remoteFilePath);
+    console.log(`File uploaded to ${remoteFilePath}`);
+
+    // 4. Листинг файлов
+    const fileList = await sftp.list("/store");
+    console.log("Files in /remote:");
+    fileList.forEach((file) => console.log(`- ${file.name} (${file.type})`));
+  } catch (error) {
+    console.error("SFTP error:", error);
+  } finally {
+    // Отключение (обязательно!)
+    await sftp.end();
+    console.log("Connection closed");
+  }
+}
+
+sftpOperations();
